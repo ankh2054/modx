0ffa0211e0bffdf409da0c5a6ea281ab8bbea2b9
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-17 14:55:09 +0100

    changed to /tmp

--- modx.sh
+++ modx.sh
@@ -6,5 +6,5 @@ wget -q http://modx.com/download/latest -O /tmp/latest.zip
 unzip /tmp/latest.zip -x "*/./" -d /tmp > /dev/null 2>&1
 rm /home/latest.zip
 MODX=`ls /tmp | grep -v latest.zip`
-mv /home/$MODX /var/www/modx
+mv /tmp/$MODX /var/www/modx
 chown -R www-data:www-data /var/www/modx
