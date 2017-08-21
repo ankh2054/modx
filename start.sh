b26f2cfb23216036a498fba6653633e7f136c368
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-21 14:42:07 +0100

    remove supervisord

--- start.sh
+++ start.sh
@@ -5,10 +5,19 @@ set -e
 chown -R www-data:www-data /DATA
 
 # init mysql db if necessary
-if [ ! -d /var/lib/mysql/mysql ];then
-    mysqld --initialize-insecure --user=root --datadir=/var/lib/mysql
-fi
+#if [ ! -d /var/lib/mysql/mysql ];then
+#    mysqld --initialize-insecure --user=root --datadir=/var/lib/mysql
+#fi
+#
+#chown -R mysql:mysql /var/lib/mysql
 
-chown -R mysql:mysql /var/lib/mysql
+# start php-fpm
+mkdir -p /DATA/logs/php-fpm
+# start nginx
+mkdir -p /DATA/logs/nginx
+mkdir -p /tmp/nginx
+chown www-data:www-data /tmp/www-data
+chown -R www-data:www-data /DATA
 
-exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
+php-fpm7
+nginx
