66ee37c0b839f0a4ecced8ab5fac550f7497a10c
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-21 11:36:16 +0100

    changed default directory to /DATA/www

--- modx.sh
+++ modx.sh
@@ -6,5 +6,5 @@ wget -q http://modx.com/download/latest -O /tmp/latest.zip
 unzip /tmp/latest.zip -x "*/./" -d /tmp > /dev/null 2>&1
 rm /home/latest.zip
 MODX=`ls /tmp | grep -v latest.zip`
-mv /tmp/$MODX /var/www/modx
-chown -R www-data:www-data /var/www/modx
+mv /tmp/$MODX /DATA/www
+chown -R www-data:www-data /DATA/www
