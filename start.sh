feb3f4afdf7e5740eb3c2a61c92474411c180665
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-23 12:10:39 +0100

    add MODX config.xml

--- start.sh
+++ start.sh
@@ -95,9 +95,46 @@ apply_www_permissions(){
 
 
 # copy MODX config files & install if file do not already exist
-if [ ! -e /DATA/www/core/config/config.inc.php ] ; then
+modx_install(){
+  echo "Installing MODX"
+  if [ ! -e /DATA/www/core/config/config.inc.php ] ; then
   cp /tmp/* /DATA/www
+
+  cat > /DATA/www/setup/config.xml <<EOF
+<modx>
+  <database_type>mysql</database_type>
+  <database_server>$MODX_DB_HOST</database_server>
+  <database>$MODX_DB_NAME</database>
+  <database_user>$MODX_DB_USER</database_user>
+  <database_password>$MODX_DB_PASSWORD</database_password>
+  <database_connection_charset>utf8</database_connection_charset>
+  <database_charset>utf8</database_charset>
+  <database_collation>utf8_general_ci</database_collation>
+  <table_prefix>modx_</table_prefix>
+  <https_port>443</https_port>
+  <http_host>localhost</http_host>
+  <cache_disabled>0</cache_disabled>
+  <inplace>1</inplace>
+  <unpacked>0</unpacked>
+  <language>en</language>
+  <cmsadmin>$MODX_ADMIN_USER</cmsadmin>
+  <cmspassword>$MODX_ADMIN_PASSWORD</cmspassword>
+  <cmsadminemail>$MODX_ADMIN_EMAIL</cmsadminemail>
+  <core_path>/DATA/www/core/</core_path>
+  <context_mgr_path>/DATA/www/manager/</context_mgr_path>
+  <context_mgr_url>/manager/</context_mgr_url>
+  <context_connectors_path>/DATA/www/connectors/</context_connectors_path>
+  <context_connectors_url>/connectors/</context_connectors_url>
+  <context_web_path>/DATA/www/</context_web_path>
+  <context_web_url>/</context_web_url>
+  <remove_setup_directory>1</remove_setup_directory>
+</modx>
+EOF
+
 fi
+
+}
+
 
 
 # Running all script functions
@@ -109,7 +146,11 @@ create_modx_database
 set_mysql_root_pw
 create_www_dir
 apply_www_permissions
+modx_install
 
 # Start Supervisor 
 echo "Starting Supervisor"
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+
+
+
