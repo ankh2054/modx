09893edb2760231aba42f8de88d8110e5da6a77c
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-23 11:45:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#
-# Install mySQL
-# https://github.com/sameersbn/docker-mysql/blob/master/entrypoint.sh
 
+
+# 1.MYSQL SETUP 
+#
+# ###########
 
 MYSQL_CHARSET=${MYSQL_CHARSET:-"utf8"}
 MYSQL_COLLATION=${MYSQL_COLLATION:-"utf8_unicode_ci"}
@@ -29,48 +30,86 @@ mysql_default_install() {
     /usr/bin/mysql_install_db --datadir=/var/lib/mysql
 }
 
-set_mysql_root_pw() {
-    /usr/bin/mysqld_safe &
-    sleep 5
-    ps axl
-    ls -l /var/lib
-    ps axl
-    /usr/bin/mysqladmin -u root password "${ROOT_PWD}"
-}
-
 create_modx_database() {
-    if [ -n "${DB_NAME}" ]; then
-        mysql -uroot --password=${ROOT_PWD} -e "CREATE DATABASE ${DB_NAME};"
+   # start mysql server.
+    /usr/bin/mysqld_safe >/dev/null 2>&1 &
+
+   # wait for mysql server to start (max 30 seconds).
+    timeout=30
+    echo -n "Waiting for database server to accept connections"
+    while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
+    do
+      timeout=$(($timeout - 1))
+      if [ $timeout -eq 0 ]; then
+        echo -e "\nCould not connect to database server. Aborting..."
+        exit 1
+      fi
+      echo -n "."
+      sleep 1
+    done
+    echo
+    
+    # create database and assign user permissions.
+    if [ -n "${DB_NAME}" -a -n "${DB_USER}" -a -n "${DB_PASS}" ]; then
+       echo "Creating database \"${DB_NAME}\" and granting access to \"${DB_USER}\" database."
+        mysql -uroot  -e  "CREATE DATABASE ${DB_NAME};"
+        mysql -uroot  -e  "GRANT USAGE ON *.* TO ${DB_USER}@localhost IDENTIFIED BY '${DB_PASS}';"
+        mysql -uroot  -e  "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@localhost;"
 
     fi 
+  
+}
+
+set_mysql_root_pw() {
+    # set root password for mysql.
+    echo "Setting root password"
+    /usr/bin/mysqladmin -u root password "${ROOT_PWD}"
+
+    # shutdown mysql reeady for supervisor to start mysql.
+    /usr/bin/mysqladmin -u root --password=${ROOT_PWD} shutdown
+}
+
+
+# 2.NGINX & PHP-FPM 
+#
+# ################
+
+create_www_dir() {
+  # Create LOG directoties for NGINX & PHP-FPM
+  echo "Creating www directories"
+  mkdir -p /DATA/logs/php-fpm
+  mkdir -p /DATA/logs/nginx
+  mkdir -p /DATA/www
 
 }
 
-create_data_dir
-create_run_dir
-create_log_dir
-mysql_default_install
-set_mysql_root_pw
-create_modx_database
+apply_www_permissions(){
+  echo "Applying www permissions"
+  chown -R www-data:www-data /DATA
+
+}
+
+# 3.MODX INSTALL  
+#
+# ################
 
 
-
-
-# Create LOG directoties for NGINX & PHP-FPM
-mkdir -p /DATA/logs/php-fpm
-mkdir -p /DATA/logs/nginx
-mkdir -p /DATA/www
-
-#Copy MODX config files & install if file do not already exist
+# copy MODX config files & install if file do not already exist
 if [ ! -e /DATA/www/core/config/config.inc.php ] ; then
   cp /tmp/* /DATA/www
 fi
 
-#Apply correct permissions
-chown -R www-data:www-data /DATA
 
-#Test to see if DB_NAME ENV is available
-echo ${DB_NAME} > /tmp/test.txt
+# Running all script functions
+create_data_dir
+create_run_dir
+create_log_dir
+mysql_default_install
+create_modx_database
+set_mysql_root_pw
+create_www_dir
+apply_www_permissions
 
-#Start Supervisor 
+# Start Supervisor 
+echo "Starting Supervisor"
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
