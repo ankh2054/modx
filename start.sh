880eb5b3a332c914ed777523c0d2ea8c3b45df59
ankh2054 <charles.holtzkampf@gmail.com>
2017-08-24 11:06:31 +0100

    changed to mysql --initialiaze-insecure

--- start.sh
+++ start.sh
@@ -9,64 +9,85 @@ MYSQL_CHARSET=${MYSQL_CHARSET:-"utf8"}
 MYSQL_COLLATION=${MYSQL_COLLATION:-"utf8_unicode_ci"}
 
 create_data_dir() {
+  echo "Creating /var/lib/mysql"
   mkdir -p /var/lib/mysql
   chmod -R 0700 /var/lib/mysql
   chown -R mysql:mysql /var/lib/mysql
 }
 
 create_run_dir() {
+  echo "Creating /run/mysqld"
   mkdir -p /run/mysqld
   chmod -R 0755 /run/mysqld
   chown -R mysql:root /run/mysqld
 }
 
 create_log_dir() {
+  echo "Creating /var/log/mysql"
   mkdir -p /var/log/mysql
   chmod -R 0755 /var/log/mysql
   chown -R mysql:mysql /var/log/mysql
 }
 
 mysql_default_install() {
-    /usr/bin/mysql_install_db --datadir=/var/lib/mysql
+  if [ ! -d "/var/lib/mysql/mysql" ]; then
+      echo "Creating the default database"
+      /usr/sbin/mysqld --initialize-insecure --datadir=/var/lib/mysql
+
+  else
+      echo "MySQL database already initialiazed"
+  fi
 }
 
 create_modx_database() {
-   # start mysql server.
-    /usr/bin/mysqld_safe >/dev/null 2>&1 &
 
-   # wait for mysql server to start (max 30 seconds).
-    timeout=30
-    echo -n "Waiting for database server to accept connections"
-    while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
-    do
-      timeout=$(($timeout - 1))
-      if [ $timeout -eq 0 ]; then
-        echo -e "\nCould not connect to database server. Aborting..."
-        exit 1
+  if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
+
+     # start mysql server.
+      echo "Starting Mysql server"
+      /usr/bin/mysqld_safe >/dev/null 2>&1 &
+
+     # wait for mysql server to start (max 30 seconds).
+      timeout=30
+      echo -n "Waiting for database server to accept connections"
+      while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
+      do
+        timeout=$(($timeout - 1))
+        if [ $timeout -eq 0 ]; then
+          echo -e "\nCould not connect to database server. Aborting..."
+          exit 1
+        fi
+        echo -n "."
+        sleep 1
+      done
+      echo
+      
+      # create database and assign user permissions.
+      if [ -n "${DB_NAME}" -a -n "${DB_USER}" -a -n "${DB_PASS}" ]; then
+         echo "Creating database \"${DB_NAME}\" and granting access to \"${DB_USER}\" database."
+          mysql -uroot  -e  "CREATE DATABASE ${DB_NAME};"
+          mysql -uroot  -e  "GRANT USAGE ON *.* TO ${DB_USER}@localhost IDENTIFIED BY '${DB_PASS}';"
+          mysql -uroot  -e  "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@localhost;"
+
       fi
-      echo -n "."
-      sleep 1
-    done
-    echo
-    
-    # create database and assign user permissions.
-    if [ -n "${DB_NAME}" -a -n "${DB_USER}" -a -n "${DB_PASS}" ]; then
-       echo "Creating database \"${DB_NAME}\" and granting access to \"${DB_USER}\" database."
-        mysql -uroot  -e  "CREATE DATABASE ${DB_NAME};"
-        mysql -uroot  -e  "GRANT USAGE ON *.* TO ${DB_USER}@localhost IDENTIFIED BY '${DB_PASS}';"
-        mysql -uroot  -e  "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@localhost;"
+  else 
+      echo "Database \"${DB_NAME}\" already exists"
 
-    fi 
+  fi
   
 }
 
 set_mysql_root_pw() {
+  if [ ! -d "/var/lib/mysql/mysql" ]; then
     # set root password for mysql.
     echo "Setting root password"
     /usr/bin/mysqladmin -u root password "${ROOT_PWD}"
 
     # shutdown mysql reeady for supervisor to start mysql.
     /usr/bin/mysqladmin -u root --password=${ROOT_PWD} shutdown
+  else 
+    echo "Default database exists, therefore root password already set"
+  fi
 }
 
 
@@ -85,7 +106,7 @@ create_www_dir() {
 
 apply_www_permissions(){
   echo "Applying www permissions"
-  chown -R www-data:www-data /DATA
+  chown -R www-data:www-data /DATA/www /DATA/logs
 
 }
 
@@ -155,8 +176,8 @@ mysql_default_install
 create_modx_database
 set_mysql_root_pw
 create_www_dir
-modx_install
 apply_www_permissions
+modx_install
 
 # Start Supervisor 
 echo "Starting Supervisor"
